841d88520477699c3592e8c9908105d05ee8dc52
Andrija <andrijagajicbusiness@gmail.com>
2021-06-24 16:57:01 +0200

    feat: start making HomePage and it's components

---
+++ src/pages/HomePages/HomePage.page.tsx
@@ -0,0 +1,11 @@
+import { HomePageContainer } from './HomePage.style';
+
+const HomePage = () => {
+    return (
+        <HomePageContainer>
+            This is home page
+        </HomePageContainer>
+    )
+}
+
+export default HomePage;
