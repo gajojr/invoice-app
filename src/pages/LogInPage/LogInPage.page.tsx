5d62c7fce96d86654e16cc2d562d7372f86841d3
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-07-08 21:46:46 +0200

    fix: redirect user to profile page if he is logged in and tries to visit login or register page

--- src/pages/LogInPage/LogInPage.page.tsx
+++ src/pages/LogInPage/LogInPage.page.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import Form from '../../components/LogInPageComponents/Form/Form.component';
 
 import { LogInPageContainer } from './LogInPage.style';
 
 const LogInPage = () => {
+    useEffect(() => {
+        if (sessionStorage.getItem('username')) {
+            window.location.href = '/profile-page';
+        }
+    }, []);
+
     return (
         <LogInPageContainer>
             <Form />
--- src/pages/RegisterPage/RegisterPage.page.tsx
+++ src/pages/RegisterPage/RegisterPage.page.tsx
@@ -1,8 +1,15 @@
+import { useEffect } from 'react';
 import { RegisterPageContainer } from './RegisterPage.style';
 
 import Form from '../../components/RegisterPageCompents/Form/Form.component';
 
 const RegisterPage = () => {
+    useEffect(() => {
+        if (sessionStorage.getItem('username')) {
+            window.location.href = '/profile-page';
+        }
+    }, []);
+
     return (
         <RegisterPageContainer>
             <Form />
