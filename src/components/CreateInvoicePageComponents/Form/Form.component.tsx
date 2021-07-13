8314279fbe64c935400f51bceaf4915037fbc6ef
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-07-13 10:35:05 +0200

    feat: user can now make invoice with services but services won't be stored in db yet

--- src/components/CreateInvoicePageComponents/Form/Form.component.tsx
+++ src/components/CreateInvoicePageComponents/Form/Form.component.tsx
@@ -2,15 +2,21 @@ import { useState } from 'react';
 
 import axios from 'axios';
 
-import { FormElement, FormCaption, StyledButton } from './Form.style';
+import { FormElement, FormCaption, StyledButton, ServicesForm, ServicePanelToggler, ServicesContainer, Service, ServiceField, RemoveIcon } from './Form.style';
 import { DatePicker, Input, Radio, message } from 'antd';
+import { CaretDownOutlined, CaretUpOutlined } from '@ant-design/icons';
 
 const Form = () => {
     const [stampValue, setStampValue] = useState<number>(1);
     const [signValue, setSignValue] = useState<number>(1);
     const [pdvValue, setPdvValue] = useState<number>(1);
+    const [servicePanelOpen, setServicePanelOpen] = useState<boolean>(false);
+    const [services, setServices] = useState<any[]>([]);
 
-    const onFinish = (values: any) => {
+    const onFinish = async (values: any) => {
+        // close services panel because of inputs
+        setServicePanelOpen(false);
+
         const data = {
             ...values,
             stamp: stampValue === 1 ? true : false,
@@ -20,31 +26,40 @@ const Form = () => {
 
         console.log(data);
 
-        axios.post('http://localhost:5000/create-invoice', data, {
+        const response = await axios.post('http://localhost:5000/create-invoice', data, {
             params: {
                 username: sessionStorage.getItem('username')
             }
         })
-            .then(response => {
-                if (response.status === 200) {
-                    message.success('Invoice created');
-                } else {
-                    message.error('Creating failed');
-                }
-            });
+
+        if (response.status === 200) {
+            message.success('Invoice created');
+        } else {
+            message.error('Creating failed');
+        }
     }
 
-    const onStampChange = (e: any) => {
-        setStampValue(e.target.value);
-    };
+    const addService = () => {
+        const type = (document.getElementById('serviceType') as HTMLInputElement).value;
+        const unit = (document.getElementById('unit') as HTMLInputElement).value;
+        const amount = (document.getElementById('amount') as HTMLInputElement).value;
+        const pricePerUnit = (document.getElementById('pricePerUnit') as HTMLInputElement).value;
 
-    const onSignChange = (e: any) => {
-        setSignValue(e.target.value);
-    };
+        setServices([...services, { type, unit, amount, pricePerUnit }]);
 
-    const onPdvChange = (e: any) => {
-        setPdvValue(e.target.value);
-    };
+        // empty all inputs for service creation
+        (document.getElementById('pricePerUnit') as HTMLInputElement).value = '';
+        (document.getElementById('unit') as HTMLInputElement).value = '';
+        (document.getElementById('amount') as HTMLInputElement).value = '';
+        (document.getElementById('pricePerUnit') as HTMLInputElement).value = '';
+    }
+
+    const removeService = (idx: number) => {
+        if (window.confirm('Do you want to remove this service?')) {
+            const newServices = services.filter((service: any, index: number) => idx !== index);
+            setServices(newServices);
+        }
+    }
 
     return (
         <FormElement onFinish={onFinish}>
@@ -128,21 +143,109 @@ const Form = () => {
                 <DatePicker />
             </FormElement.Item>
 
-            <Radio.Group onChange={onStampChange} value={stampValue} style={{ margin: 5 }}>
+            <ServicePanelToggler>Add service
+                {
+                    servicePanelOpen
+                        ? <CaretUpOutlined onClick={() => setServicePanelOpen(!servicePanelOpen)} style={{ fontSize: '32px', color: '#08c' }} />
+                        : <CaretDownOutlined onClick={() => setServicePanelOpen(!servicePanelOpen)} style={{ fontSize: '32px', color: '#08c' }} />
+                }
+            </ServicePanelToggler>
+
+            <ServicesForm style={{ display: `${servicePanelOpen ? 'flex' : 'none'}` }}>
+                <FormElement.Item
+                    label="Service type:"
+                    name="serviceType"
+                    id="serviceType"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter the type of service!',
+                        }
+                    ]}
+                >
+                    <Input />
+                </FormElement.Item>
+
+                <FormElement.Item
+                    label="Unit:"
+                    name="unit"
+                    id="unit"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter the unit!',
+                        }
+                    ]}
+                >
+                    <Input />
+                </FormElement.Item>
+
+                <FormElement.Item
+                    label="Amount:"
+                    name="amount"
+                    id="amount"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter the amount!',
+                        }
+                    ]}
+                >
+                    <Input />
+                </FormElement.Item>
+
+                <FormElement.Item
+                    label="Price per unit:"
+                    name="pricePerUnit"
+                    id="pricePerUnit"
+                    rules={[
+                        {
+                            required: true,
+                            message: 'Please enter the price per unit!',
+                        }
+                    ]}
+                >
+                    <Input />
+                </FormElement.Item>
+
+                <StyledButton onClick={() => {
+                    addService();
+                    setServicePanelOpen(!servicePanelOpen);
+                }}>Add service</StyledButton>
+            </ServicesForm>
+
+            {
+                services.length
+                    ? <ServicesContainer>
+                        <p>Services:</p>
+                        {services.map((service: any, idx: number) => (
+                            <Service key={idx}>
+                                <ServiceField>{service.type}</ServiceField>
+                                <ServiceField>{service.unit}</ServiceField>
+                                <ServiceField>{service.amount}</ServiceField>
+                                <ServiceField>{service.pricePerUnit}</ServiceField>
+                                <RemoveIcon onClick={() => removeService(idx)} />
+                            </Service>
+                        ))}
+                    </ServicesContainer>
+                    : null
+            }
+
+            <Radio.Group onChange={(e: any) => setStampValue(e.target.value)} value={stampValue} style={{ margin: 5 }}>
                 <hr />
                 <Radio value={1}>Stamp needed</Radio>
                 <Radio value={2}>Stamp isn't needed</Radio>
                 <hr />
             </Radio.Group>
 
-            <Radio.Group onChange={onSignChange} value={signValue} style={{ margin: 5 }}>
+            <Radio.Group onChange={(e: any) => setSignValue(e.target.value)} value={signValue} style={{ margin: 5 }}>
                 <hr />
                 <Radio value={1}>Sign needed</Radio>
                 <Radio value={2}>Sign isn't needed</Radio>
                 <hr />
             </Radio.Group>
 
-            <Radio.Group onChange={onPdvChange} value={pdvValue} style={{ margin: 5 }}>
+            <Radio.Group onChange={(e: any) => setPdvValue(e.target.value)} value={pdvValue} style={{ margin: 5 }}>
                 <hr />
                 <Radio value={1}>Tax gatherer is in pdv system</Radio>
                 <Radio value={2}>Tax gatherer isn't in pdv system</Radio>
--- src/components/CreateInvoicePageComponents/Form/Form.style.ts
+++ src/components/CreateInvoicePageComponents/Form/Form.style.ts
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 
 import { Form, Button } from 'antd';
+import { CloseCircleOutlined } from '@ant-design/icons';
 
 export const FormElement = styled(Form)`
     display: flex;
@@ -19,4 +20,42 @@ export const StyledButton = styled(Button)`
     margin: 5px;
     width: 70%;
     align-self: center;
+`;
+
+export const ServicePanelToggler = styled.span`
+    display: flex;
+    align-items: center;
+`;
+
+export const ServicesForm = styled.div`
+    display: flex;
+    flex-direction: column;
+`;
+
+export const ServicesContainer = styled.div`
+    display: flex;
+    flex-direction: column;
+    width: 100%;
+`;
+
+export const Service = styled.div`
+    display: flex;
+    width: 100%;
+`;
+
+export const ServiceField = styled.span`
+    border: 1px solid #000;
+    padding: 2px;
+    flex: 1;
+`;
+
+export const RemoveIcon = styled(CloseCircleOutlined)`
+    font-size: 24px;
+    margin-left: 5px;
+    color: red;
+    align-self: center;
+
+    &:hover {
+        cursor: pointer;
+    }
 `;
