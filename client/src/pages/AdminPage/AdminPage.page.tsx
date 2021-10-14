93ee503c8599320a967f68e7ab2e69cc4eeb468e
Andrija Gajic <andrijagajicbusiness@gmail.com>
2021-10-14 15:13:51 +0200

    fix: fix typo on admin page

--- client/src/pages/AdminPage/AdminPage.page.tsx
+++ client/src/pages/AdminPage/AdminPage.page.tsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react';
 
 import DeleteUserComponent from '../../components/AdminPageComponents/DeleteUserComponent/DeleteUser.component';
-import UpdateUserComponent from '../../components/AdminPageComponents/UpdateUsesComponent/UpdateUser.component';
+import UpdateUserComponent from '../../components/AdminPageComponents/UpdateUserComponent/UpdateUser.component';
 import NavigationComponent from '../../components/AdminPageComponents/Navigation/Navigation.component';
 
 import { AdminPageContainer } from './AdminPage.style';
